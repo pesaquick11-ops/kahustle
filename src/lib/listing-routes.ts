fda084e8cfb49d55993be264c544a0e4a4d37549
madegwa-o <madegwaoscar317@gmail.com>
2026-05-15 20:57:39 +0300

    new history

--- src/lib/listing-routes.ts
+++
@@ -1,9 +0,0 @@
-import { CATEGORY_REGISTRY, type CategoryKey } from "@/lib/categories"
-
-export function getListingDetailRoute(categoryKey: CategoryKey, listingId: string): string {
-  return `${CATEGORY_REGISTRY[categoryKey].detailRoute}/${listingId}`
-}
-
-export function getCategoryRoute(categoryKey: CategoryKey): string {
-  return CATEGORY_REGISTRY[categoryKey].routeBase
-}
